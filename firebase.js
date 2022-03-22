345c714b7554638ab7b55adedecb791b9edb1b44
JMDoza <Mendozaji327@gmail.com>
2022-03-21 20:14:49 -0400

    Basic User Authentication

---
+++ firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app'
+import "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdDRhjG0ueUWyF8m0rMXVorFY_cz-bhzY",
+  authDomain: "mobile-passport-for-success.firebaseapp.com",
+  projectId: "mobile-passport-for-success",
+  storageBucket: "mobile-passport-for-success.appspot.com",
+  messagingSenderId: "999142558151",
+  appId: "1:999142558151:web:30fd83b8e33e8bad09b1e2",
+  measurementId: "G-4XWJW9YV9Q"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
---
+++ screens/LoginScreen.js
@@ -0,0 +1,133 @@
+import { StyleSheet, Text, TextInput, View, KeyboardAvoidingView, TouchableOpacity, } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/native'
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigation.replace("Home")
+      }
+    })
+
+    return unsubscribe
+  }, [])
+
+  const handleSignUp = () => {
+    auth
+    .createUserWithEmailAndPassword(email,password)
+    .then(userCredentials => {
+      const user = userCredentials.user;
+      // console.log(user.email);
+    })
+    .catch(error => alert(error.message))
+  }
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Logged in with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      //behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Email'
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Password'
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782f9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782f9',
+    borderWidth: 2,
+
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16
+  },
+  buttonOutlineText: {
+    color: '#0782f9',
+    fontWeight: '700',
+    fontSize: 16
+  },
+})
