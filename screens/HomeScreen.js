f9c7d456d3d2f7d4c985fb3c9de3c4456eab7c3a
Jesse Kostoss <34844169+JWKostoss@users.noreply.github.com>
2022-03-26 14:44:40 -0400

    navigation
    adding the screen navigation code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,6 +16,30 @@ const HomeScreen = () => {
       .catch(error => alert(error.message))
   }
 
+  const handleDocuments = () => {
+    navigation.navigate('Documents')
+  }
+
+  const handleEducation = () => {
+    navigation.navigate('Education')
+  }
+
+  const handleEmployment = () => {
+    navigation.navigate('Employment')
+  }
+
+  const handleFinancial = () => {
+    navigation.navigate('Financial')
+  }
+
+  const handleHealthcare = () => {
+    navigation.navigate('Healthcare')
+  }
+
+  const handleHousing = () => {
+    navigation.navigate('Housing')
+  }
+
   return (
     <View style={styles.container}>
       <Text>Email: {auth.currentUser?.email}</Text>
@@ -24,6 +48,54 @@ const HomeScreen = () => {
         style={styles.button}
       >
         <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+
+{/* documents button */}
+    <TouchableOpacity
+    onPress={handleDocuments}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Documents</Text>
+      </TouchableOpacity>
+
+{/* education button */}
+<TouchableOpacity
+    onPress={handleEducation}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Education</Text>
+      </TouchableOpacity>
+
+{/* employment button */}
+<TouchableOpacity
+    onPress={handleEmployment}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Employment</Text>
+      </TouchableOpacity>
+
+{/* financial button */}
+<TouchableOpacity
+    onPress={handleFinancial}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Financial Literacy</Text>
+      </TouchableOpacity>
+
+{/* healthcare button */}
+<TouchableOpacity
+    onPress={handleHealthcare}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Healthcare</Text>
+      </TouchableOpacity>
+
+{/* housing button */}
+<TouchableOpacity
+    onPress={handleHousing}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Housing</Text>
       </TouchableOpacity>
     </View>
   )
@@ -49,5 +121,21 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
+  },
+  circleButton: {
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    borderWidth:3,
+    borderColor:'black',
+    backgroundColor: '#c5b358',
+  },
+  circleText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 12,
   },
 })
