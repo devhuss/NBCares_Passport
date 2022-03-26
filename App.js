df118cb62d80579c29096a305ea2e1e4f169c51f
Jesse Kostoss <34844169+JWKostoss@users.noreply.github.com>
2022-03-26 14:42:40 -0400

    navigation
    This is adding the screens to be interacted from the homescreen

--- App.js
+++ App.js
@@ -4,6 +4,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import SignUpScreen from './screens/SignUpScreen';
+import DocumentsScreen from './screens/tabs/documents';
+import HousingScreen from './screens/tabs/housing';
+import EducationScreen from './screens/tabs/education';
+import EmploymentScreen from './screens/tabs/employment';
+import FinancialScreen from './screens/tabs/financial';
+import HealthcareScreen from './screens/tabs/healthcare';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +19,12 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen options={{headerShown: false}} name="SignUp" component={SignUpScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Documents" component={DocumentsScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Housing" component={HousingScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Education" component={EducationScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Employment" component={EmploymentScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Financial" component={FinancialScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Healthcare" component={HealthcareScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -27,4 +39,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
