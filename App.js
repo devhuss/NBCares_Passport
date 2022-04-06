1eb5985e96e1c00622e82baf3400cb88513b3658
Jesse Kostoss <34844169+JWKostoss@users.noreply.github.com>
2022-04-06 00:27:59 -0400

    Add files via upload
    pretty sure this is everything

--- App.js
+++ App.js
@@ -4,13 +4,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import SignUpScreen from './screens/SignUpScreen';
-import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import DocumentsScreen from './screens/tabs/documents';
 import HousingScreen from './screens/tabs/housing';
 import EducationScreen from './screens/tabs/education';
 import EmploymentScreen from './screens/tabs/employment';
 import FinancialScreen from './screens/tabs/financial';
 import HealthcareScreen from './screens/tabs/healthcare';
+import VitalScreen from './screens/tabs/vital';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,14 +18,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
-        <Stack.Screen options={{headerShown: true}} name="Register" component={SignUpScreen} />
+        <Stack.Screen options={{headerShown: true}} name="Login" component={LoginScreen} />
+        <Stack.Screen options={{headerShown: true}} name="SignUp" component={SignUpScreen} />
         <Stack.Screen options={{headerShown: true}} name="Documents" component={DocumentsScreen} />
         <Stack.Screen options={{headerShown: true}} name="Housing" component={HousingScreen} />
         <Stack.Screen options={{headerShown: true}} name="Education" component={EducationScreen} />
         <Stack.Screen options={{headerShown: true}} name="Employment" component={EmploymentScreen} />
         <Stack.Screen options={{headerShown: true}} name="Financial" component={FinancialScreen} />
         <Stack.Screen options={{headerShown: true}} name="Healthcare" component={HealthcareScreen} />
+        <Stack.Screen options={{headerShown: true}} name="Vital Signs" component={VitalScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -35,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,142 +1,169 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  SafeAreaView,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
-import React, { useState, useEffect } from "react";
-import { Fire } from "../firebase";
-import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/native'
+//import Header from '../screens/header' //this is supposed to help with header navigation for menu(later)
 
 const HomeScreen = () => {
 
-  //console.log('==================================================')
-  const [lists, setLists] = useState([]);
-  const [loading, setLoading] = useState(true);
-  //console.log('INITIALIZE: ' + loading)
-
-  // Initialize Firebase class to use methods 
-  const fire = new Fire();
-
-  // useEffect is a Effect hook that triggers depending on render
-  // this useEffect triggers once when HomeScreen renders, when triggered it calls the firebase getLists function
-  // to retrieve the data from the Database, after the cleanup function is called to unsubscribe to the firebase
-  // listener that recieves the data
-  useEffect(() => {
-    fire.getLists((lists) => {
-
-      setLists(lists)
-      //console.log('GET LIST CALL: ' + loading)
-    })
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      //console.log('CLEAN UP CALL')
-      //console.log(lists)
-      fire.detach()
-    };
-  }, []);
-
-  // This useEffect triggers when the useState 'lists' changes, when it changes the loading state to false 
-  // so that the activity indicator is disabled and shows the content of the screen when loaded
-
-  // At least thats how it should work but currently the useEffect is triggered on the initialization of the useState
-  // this can be considered a bug
-  useEffect(() => {
-    //console.log('LISTCHANGED ' + loading)
-    setLoading(false)
-  }, [lists])
-
-  const navigation = useNavigation();
-
+  const navigation = useNavigation()
+  
   const handleSignOut = () => {
-    fire.auth
+    auth
       .signOut()
       .then(() => {
-        navigation.replace("Login");
+        navigation.replace("Login")
       })
-      .catch((error) => alert(error.message));
-  };
-
-  const ItemRender = ({ item, name }) => (
-    <TouchableOpacity
-      style={styles.circleButton}
-      onPress={() => navigation.navigate(name, { list: item, fire: fire })}
-    >
-      <Text style={styles.circleText}>{name}</Text>
-    </TouchableOpacity>
-  );
-
-  // If loading is true then display activity indicator
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size='large' color={'blue'} />
-      </View>
-    )
+      .catch(error => alert(error.message))
   }
+
+  const handleDocuments = () => {
+    navigation.navigate('Documents')
+  }
+
+  const handleEducation = () => {
+    navigation.navigate('Education')
+  }
+
+  const handleEmployment = () => {
+    navigation.navigate('Employment')
+  }
+
+  const handleFinancial = () => {
+    navigation.navigate('Financial')
+  }
+
+  const handleHealthcare = () => {
+    navigation.navigate('Healthcare')
+  }
+
+  const handleHousing = () => {
+    navigation.navigate('Housing')
+  }
+
+  const handleVital = () => {
+    navigation.navigate('Vital Signs')
+  }
+
+  //this does not work
+  var disableButtons = true;
 
   return (
 
-    <SafeAreaView style={styles.container}>
-      <Text>Email: {fire.auth.currentUser?.email}</Text>
-      <Text>userID: {fire.userID}</Text>
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+      onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
 
+{/* this will enable the buttons. I'm doing this so its easier to get our giant bubble later
+    however for some reason "disable buttons wants to stay false, so you can press anyways" */}
+<TouchableOpacity
+    //this is apparently not an expression
+    onPress={disableButtons=false}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Enable Tabs</Text>
+      </TouchableOpacity>
 
-      <FlatList
-        data={lists}
-        renderItem={({ item }) => <ItemRender item={item} name={item.name} />}
-        keyExtractor={(item) => item.id.toString()}
-      />
+{/* documents button */}
+    <TouchableOpacity disabled={disableButtons}
+    onPress={handleDocuments}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Documents</Text>
+      </TouchableOpacity>
 
-      <View>
-        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-          <Text style={styles.buttonText}>Sign out</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+{/* education button */}
+<TouchableOpacity disabled={disableButtons}
+    onPress={handleEducation}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Education</Text>
+      </TouchableOpacity>
 
-  );
-};
+{/* employment button */}
+<TouchableOpacity disabled={disableButtons}
+    onPress={handleEmployment}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Employment</Text>
+      </TouchableOpacity>
 
-export default HomeScreen;
+{/* financial button */}
+<TouchableOpacity disabled={disableButtons}
+    onPress={handleFinancial}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Financial Literacy</Text>
+      </TouchableOpacity>
+
+{/* healthcare button */}
+<TouchableOpacity disabled={disableButtons}
+    onPress={handleHealthcare}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Healthcare</Text>
+      </TouchableOpacity>
+
+{/* housing button */}
+<TouchableOpacity disabled={disableButtons}
+    onPress={handleHousing}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Housing</Text>
+      </TouchableOpacity>
+
+{/* vital signs temporary button just to see the screen*/}
+<TouchableOpacity disabled={disableButtons}
+    onPress={handleVital}
+      style={styles.circleButton}
+      >
+        <Text style={styles.circleText}>Vital Signs</Text>
+      </TouchableOpacity>
+
+    </View>
+  )
+}
+
+export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  button: {
-    //backgroundColor: 'blue',
+   button: {
+    backgroundColor: '#0782F9',
     width: '60%',
     padding: 15,
     borderRadius: 10,
-    alignItems: "center",
+    alignItems: 'center',
     marginTop: 40,
-    bottom: 30,
   },
   buttonText: {
-    color: 'lightblue',
+    color: 'white',
     fontWeight: '700',
     fontSize: 16,
   },
   circleButton: {
     width: 70,
     height: 70,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 10,
     borderRadius: 100,
-    borderWidth: 3,
-    borderColor: "black",
-    backgroundColor: "#c5b358",
+    borderWidth:3,
+    borderColor:'black',
+    backgroundColor: '#c5b358',
   },
   circleText: {
-    color: "white",
-    fontWeight: "700",
+    color: 'white',
+    fontWeight: '700',
     fontSize: 12,
   },
-});
+})
