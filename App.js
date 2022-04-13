013fc29838f5056333b9e32c7c532805abe746ec
devhuss <mohamoudhussein95@gmail.com>
2022-04-13 16:13:43 -0400

    Navigation(s): Implemented Tab & Drawer navigation.

--- App.js
+++ App.js
@@ -2,20 +2,13 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
-import DocumentsScreen from './screens/tabs/documents';
-import HousingScreen from './screens/tabs/housing';
-import EducationScreen from './screens/tabs/education';
-import EmploymentScreen from './screens/tabs/employment';
-import FinancialScreen from './screens/tabs/financial';
-import HealthcareScreen from './screens/tabs/healthcare';
-import InformationScreen from './screens/InformationScreen';
-import VitalScreen from "./screens/tabs/vital";
 import { Fire } from "./firebase";
 import { PageContext } from "./context";
+import 'react-native-gesture-handler';
+import DrawerNavigator from "./navigation/DrawerNavigator";
+
+
+
 
 
 const Stack = createNativeStackNavigator();
@@ -72,60 +65,11 @@ export default function App() {
         pointss: [points, setPoints],
       }}
     >
+      {/* <NavigationContainer>
+        <AppStack/></NavigationContainer> */}
+      
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            options={{ headerShown: false }}
-            name="Login"
-            component={LoginScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Register"
-            component={SignUpScreen}
-          />
-          <Stack.Screen
-            options={{headerShown: false}} 
-            name="Information" 
-            component={InformationScreen} 
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Documents"
-            component={DocumentsScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Housing"
-            component={HousingScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Education"
-            component={EducationScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Employment"
-            component={EmploymentScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Financial"
-            component={FinancialScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Healthcare"
-            component={HealthcareScreen}
-          />
-          <Stack.Screen
-            options={{ headerShown: true }}
-            name="Vital Signs"
-            component={VitalScreen}
-          />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
+      <DrawerNavigator/>
       </NavigationContainer>
     </PageContext.Provider>
   );
---
+++ navigation/MainStackNavigator.js
@@ -0,0 +1,94 @@
+import React from "react";
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import DocumentsScreen from '../screens/tabs/documents';
+import HousingScreen from '../screens/tabs/housing';
+import EducationScreen from '../screens/tabs/education';
+import EmploymentScreen from '../screens/tabs/employment';
+import FinancialScreen from '../screens/tabs/financial';
+import HealthcareScreen from '../screens/tabs/healthcare';
+import InformationScreen from '../screens/InformationScreen';
+import VitalScreen from "../screens/tabs/vital";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+const Stack = createNativeStackNavigator();
+
+const screenOptionStyle = {
+  headerStyle: {
+    backgroundColor: "#859a9b",
+  },
+  headerTintColor: "white",
+  headerBackTitle: "Back",
+};
+
+const MainStackNavigator = () => {
+    return(
+<Stack.Navigator screenOptions={screenOptionStyle}>
+<Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen 
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            
+            name="Register"
+            component={SignUpScreen}
+          />
+          <Stack.Screen
+            
+            name="Information" 
+            component={InformationScreen} 
+          />
+          <Stack.Screen
+            
+            name="Documents"
+            component={DocumentsScreen}
+          />
+          <Stack.Screen
+          
+            name="Housing"
+            component={HousingScreen}
+          />
+          <Stack.Screen
+           
+            name="Education"
+            component={EducationScreen}
+          />
+          <Stack.Screen
+            
+            name="Employment"
+            component={EmploymentScreen}
+          />
+          <Stack.Screen
+            
+            name="Financial"
+            component={FinancialScreen}
+          />
+          <Stack.Screen
+            
+            name="Healthcare"
+            component={HealthcareScreen}
+          />
+          <Stack.Screen
+            
+            name="Vital Signs"
+            component={VitalScreen}
+          />
+          
+ </Stack.Navigator>
+    );
+
+    
+}
+const VitalStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Vital Signs" component={VitalScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export { MainStackNavigator, VitalStackNavigator };
