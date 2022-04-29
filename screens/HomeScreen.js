b7292c95301f3a31278a92ce1724d279bd1d241a
Jesse Kostoss <34844169+JWKostoss@users.noreply.github.com>
2022-04-28 22:38:04 -0400

    removed more unused code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,10 +6,7 @@ import {
   SafeAreaView,
   FlatList,
   ActivityIndicator,
-  Image,
-  ScrollView,
   TouchableWithoutFeedback,
-  Animated,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -29,27 +26,6 @@ const HomeScreen = () => {
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
   const [header, setHeader] = headers;
-
-
-  //const test = new Date(vitalsigns[0].createdAt.toDate()).toString()
-
-  //console.log("HOME: ", vitalsigns);
-
-  const increasePoints = () => {
-    setPoints(points + 1);
-    fire.updatePoints({
-      userPoints: points + 1,
-    });
-    setRefresh(!refresh);
-  };
-
-  const decreasePoints = () => {
-    setPoints(points - 1);
-    fire.updatePoints({
-      userPoints: points - 1,
-    });
-    setRefresh(!refresh);
-  };
 
   // Sets the authID useState to the user ID from firebase, this then allows the useEffect in App.js to trigger
   // and retieve the lists and user points
@@ -77,21 +53,6 @@ const HomeScreen = () => {
       .catch((error) => alert(error.message));
   };
 
-  const handleVital = () => {
-    navigation.navigate("Vital Signs");
-  };
-
-  // If loading is true then display activity indicator
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator size="large" color={"blue"} />
-      </View>
-    );
-  }
-
-  // these functions is to toggle the center button. it will be the animation
-
   return (
     <SafeAreaView style={[styles.container,{bottom:230}]}>
       {/* functional homescreen button call */}
@@ -111,7 +72,6 @@ const HomeScreen = () => {
   );
 }
 
-
 export default HomeScreen;
 
 const styles = StyleSheet.create({
@@ -119,51 +79,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  button: {
-    backgroundColor: 'blue',
-    width: "90%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-    marginTop: 40,
-    top:125
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-  pointsText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 60,
-  },
-  circleButton: {
-    width: 70,
-    height: 70,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-    borderRadius: 100,
-    borderWidth: 3,
-    borderColor: "black",
-    backgroundColor: "#c5b358",
-  },
-  circleText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 12,
-  },
-  largeCircle: {
-    width: 175,
-    height: 175,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-    borderRadius: 100,
-    borderWidth: 3,
-    borderColor: "black",
-    backgroundColor: "#c5b358",
   },
 });
